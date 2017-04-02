30e2d62d18f97a3a12ea435fd1011a725c09c034
Nimrod Goldrat <nimrodg@cityhive.net>
2017-04-02 20:00:07 +0300

    hardcoded to use mongoid adapter

--- lib/ransack/adapters.rb
+++ lib/ransack/adapters.rb
@@ -6,11 +6,7 @@ module Ransack
     end
 
     def self.instantiate_object_mapper
-      if defined?(::ActiveRecord::Base)
-        ActiveRecordAdapter.new
-      elsif defined?(::Mongoid)
-        MongoidAdapter.new
-      end
+      MongoidAdapter.new
     end
 
     class ActiveRecordAdapter
